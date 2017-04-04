49383e81c687a04017750e85491d9f0e80731e04
danielhogg <danhogg17@gmail.com>
2017-04-04 10:46:34 -0400

    open cv project

--- core/src/opencv_demo/opencv/main.cpp
+++ core/src/opencv_demo/opencv/main.cpp
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+using namespace std;
 using namespace cv;
 
 string type2str(int type) {
@@ -30,11 +32,10 @@ int main()
 {
     Mat image;// new blank image
     image = cv::imread("test.png", 0);// read the file
-    string imageStringOutput = type2str(image.type())
-    printf(imageStringOutput);
+    string ty = type2str(image.type());
+
     namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// create a window for display.
     imshow( "Display window", image );// show our image inside it.
     waitKey(0);// wait for a keystroke in the window
     return 0;
 }
-
