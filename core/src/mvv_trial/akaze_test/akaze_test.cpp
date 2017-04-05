5fbc9e748ceebf9be12c59a0e77902158807ef9c
danielhogg <danhogg17@gmail.com>
2017-04-05 11:41:10 -0400

    Relative paths added

--- core/src/mvv_trial/akaze_test/akaze_test.cpp
+++ core/src/mvv_trial/akaze_test/akaze_test.cpp
@@ -14,27 +14,5 @@ int main()
 
 	waitKey(0);
 
-	Mat image1, image2;
-	image1 = imread("C:\\Users\\Danny\\Documents\\GitHub\\mvv\\core\\src\\mvv_trial\\data_store\\lena.bmp", CV_LOAD_IMAGE_COLOR);
-	if (!image1.data)
-	{
-		cout << "Could not open or find the image" << endl;
-		return -1;
-	}
-
-	image2 = imread("C:\\Users\\Danny\\Documents\\GitHub\\mvv\\core\\src\\mvv_trial\\data_store\\lena.bmp", CV_LOAD_IMAGE_GRAYSCALE);
-	if (!image2.data)
-	{
-		cout << "Could not open or find the image" << endl;
-		return -1;
-	}
-
-	cout << "opencv test" << endl;
-	namedWindow("Color Image", WINDOW_AUTOSIZE);
-	imshow("Color Image", WINDOW_AUTOSIZE);
-	namedWindow("Gray Scale Image", WINDOW_AUTOSIZE);
-	imshow("Gray Scale Image", image2);
-	waitKey(0);
-	destroyAllWindows();
 	return 0;
 }
--- core/src/mvv_trial/mvv_trial/mvv_trial.cpp
+++ core/src/mvv_trial/mvv_trial/mvv_trial.cpp
@@ -5,24 +5,26 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+
 using namespace cv;
 using namespace std;
 int main()
 {
+
 
 	cout << "Program started" << endl;
 
 	waitKey(0);
 
 	Mat image1, image2;
-	image1 = imread("C:\\Users\\Danny\\Documents\\GitHub\\mvv\\core\\src\\mvv_trial\\data_store\\lena.bmp", CV_LOAD_IMAGE_COLOR);
+	image1 = imread("..\\data_store\\lena.bmp", CV_LOAD_IMAGE_COLOR);
 	if (!image1.data)
 	{
 		cout << "Could not open or find the image" << endl;
 		return -1;
 	}
 
-	image2 = imread("C:\\Users\\Danny\\Documents\\GitHub\\mvv\\core\\src\\mvv_trial\\data_store\\lena.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+	image2 = imread("..\\data_store\\lena.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 	if (!image2.data)
 	{
 		cout << "Could not open or find the image" << endl;
