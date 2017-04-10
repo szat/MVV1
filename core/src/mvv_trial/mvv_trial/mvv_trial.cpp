db00cf72170b00de6dc4172bd5aed9866804b1ed
danielhogg <danhogg17@gmail.com>
2017-04-10 16:09:19 -0400

    Triangulation with static random values

--- core/src/mvv_trial/mvv_trial/mvv_trial.cpp
+++ core/src/mvv_trial/mvv_trial/mvv_trial.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <ctime>
 
 using namespace cv;
 using namespace std;
@@ -32,7 +33,9 @@ static void draw_subdiv(Mat& img, Subdiv2D& subdiv, Scalar delaunay_color)
 	subdiv.getTriangleList(triangleList);
 	vector<Point> pt(3);
 
-	for (size_t i = 0; i < triangleList.size(); i++)
+	int numTriangles = triangleList.size();
+
+	for (size_t i = 0; i < numTriangles; i++)
 	{
 		Vec6f t = triangleList[i];
 		pt[0] = Point(cvRound(t[0]), cvRound(t[1]));
@@ -77,6 +80,61 @@ static void locate_point(Mat& img, Subdiv2D& subdiv, Point2f fp, Scalar active_c
 	draw_subdiv_point(img, fp, active_color);
 }
 
+vector<Point2f> get_sample_points() {
+	vector<Point2f> points = vector<Point2f>();
+	points.push_back(Point2f(528, 390));
+	points.push_back(Point2f(551, 209));
+	points.push_back(Point2f(289, 355));
+	points.push_back(Point2f(569, 105));
+	points.push_back(Point2f(155, 551));
+	points.push_back(Point2f(335, 319));
+	points.push_back(Point2f(273, 331));
+	points.push_back(Point2f(353, 505));
+	points.push_back(Point2f(590, 287));
+	points.push_back(Point2f(305, 31));
+	points.push_back(Point2f(25, 313));
+	points.push_back(Point2f(16, 374));
+	points.push_back(Point2f(44, 57));
+	points.push_back(Point2f(517, 335));
+	points.push_back(Point2f(564, 475));
+	points.push_back(Point2f(508, 548));
+	points.push_back(Point2f(126, 494));
+	points.push_back(Point2f(553, 267));
+	points.push_back(Point2f(52, 565));
+	points.push_back(Point2f(91, 589));
+	points.push_back(Point2f(234, 478));
+	points.push_back(Point2f(179, 211));
+	points.push_back(Point2f(261, 324));
+	points.push_back(Point2f(392, 91));
+	points.push_back(Point2f(560, 592));
+	points.push_back(Point2f(578, 383));
+	points.push_back(Point2f(260, 316));
+	points.push_back(Point2f(529, 14));
+	points.push_back(Point2f(70, 507));
+	points.push_back(Point2f(264, 477));
+	points.push_back(Point2f(188, 119));
+	points.push_back(Point2f(156, 125));
+	points.push_back(Point2f(207, 251));
+	points.push_back(Point2f(162, 214));
+	points.push_back(Point2f(218, 592));
+	points.push_back(Point2f(504, 38));
+	points.push_back(Point2f(319, 267));
+	points.push_back(Point2f(21, 31));
+	points.push_back(Point2f(106, 88));
+	points.push_back(Point2f(508, 158));
+	points.push_back(Point2f(482, 158));
+	points.push_back(Point2f(419, 335));
+	points.push_back(Point2f(332, 554));
+	points.push_back(Point2f(538, 589));
+	points.push_back(Point2f(133, 198));
+	points.push_back(Point2f(160, 9));
+	points.push_back(Point2f(436, 462));
+	points.push_back(Point2f(256, 442));
+	points.push_back(Point2f(159, 130));
+	points.push_back(Point2f(97, 150));
+	return points;
+}
+
 int main(int argc, char** argv)
 {
 	cv::CommandLineParser parser(argc, argv, "{help h||}");
@@ -94,9 +152,33 @@ int main(int argc, char** argv)
 
 	img = Scalar::all(0);
 	string win = "Delaunay Demo";
-	imshow(win, img);
+	//imshow(win, img);
 
-	for (int i = 0; i < 1000; i++)
+	std::clock_t start;
+	double duration;
+	start = std::clock();
+
+
+	vector<Point2f> points = get_sample_points();
+	int numPoints = points.size();
+
+	for (int i = 0; i < numPoints; i++) {
+		locate_point(img, subdiv, points[i], active_facet_color);
+		imshow(win, img);
+
+		waitKey(1);
+
+		subdiv.insert(points[i]);
+
+		img = Scalar::all(0);
+		draw_subdiv(img, subdiv, delaunay_color);
+		imshow(win, img);
+
+		waitKey(1);
+	}
+
+	/*
+	for (int i = 0; i < 200; i++)
 	{
 		Point2f fp((float)(rand() % (rect.width - 10) + 5),
 			(float)(rand() % (rect.height - 10) + 5));
@@ -114,10 +196,14 @@ int main(int argc, char** argv)
 
 		waitKey(1);
 	}
+	*/
 
 	img = Scalar::all(0);
 	//paint_voronoi(img, subdiv);
 	imshow(win, img);
+
+	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+	std::cout << "printf: " << duration << '\n';
 
 	cin.get();
 
