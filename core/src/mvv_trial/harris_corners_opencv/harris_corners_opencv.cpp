c5d0c204074e65336ed548511c2c930aac64119c
szat <artabalt@gmail.com>
2017-04-11 16:40:08 -0400

    good features detector (harris/non-harris) working

--- core/src/mvv_trial/harris_corners_opencv/harris_corners_opencv.cpp
+++ core/src/mvv_trial/harris_corners_opencv/harris_corners_opencv.cpp
@@ -1,49 +1,77 @@
-// harris_corners_opencv.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
 using namespace cv;
 using namespace std;
-
+Mat src, src_gray;
+int maxCorners = 1000;
+int maxTrackbar = 1000;
+RNG rng(12345);
+const char* source_window = "Image";
+void goodFeaturesToTrack_Demo(int, void*);
 int main(int, char** argv)
 {
-	int thresh = 200;
-	Mat src, gray;
-	// Load source image and convert it to gray
-	src = imread("..\\data_store\\mona_lisa_1.jpg", 1);
-	cvtColor(src, gray, CV_BGR2GRAY);
-	Mat dst, dst_norm, dst_norm_scaled;
-	dst = Mat::zeros(src.size(), CV_32FC1);
+	src = imread("..\\data_store\\david_1.jpg", IMREAD_COLOR);
+	cvtColor(src, src_gray, COLOR_BGR2GRAY);
 
-	// Detecting corners
-	cornerHarris(gray, dst, 20, 9, 0.05, BORDER_DEFAULT);
+	/*
+	namedWindow(source_window, WINDOW_AUTOSIZE);
+	createTrackbar("Max  corners:", source_window, &maxCorners, maxTrackbar, goodFeaturesToTrack_Demo);
+	imshow(source_window, src);
 
-	// Normalizing
-	normalize(dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
-	convertScaleAbs(dst_norm, dst_norm_scaled);
+	//For interactive
+	//goodFeaturesToTrack_Demo(0, 0);
+	*/
 
-	// Drawing a circle around corners
-	for (int j = 0; j < dst_norm.rows; j++)
+	vector<Point2f> corners;
+	double qualityLevel = 0.01; //the higher, the less points
+	double minDistance = 10;
+	int blockSize = 3; //not sure of this effect
+	bool useHarrisDetector = false; 
+	double k =  0.04; //not sure of this effect
+	Mat copy;
+	copy = src.clone();
+	goodFeaturesToTrack(src_gray, corners, maxCorners, qualityLevel, minDistance, Mat(), blockSize, useHarrisDetector, k);
+	cout << "** Number of corners detected: " << corners.size() << endl;
+	int r = 4;
+	for (size_t i = 0; i < corners.size(); i++)
 	{
-		for (int i = 0; i < dst_norm.cols; i++)
-		{
-			if ((int)dst_norm.at<float>(j, i) > thresh)
-			{
-				circle(dst_norm_scaled, Point(i, j), 5, Scalar(0), 2, 8, 0);
-			}
-		}
+		circle(copy, corners[i], r, Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)), -1, 8, 0);
 	}
-
-
-	// Showing the result
-	namedWindow("corners_window", CV_WINDOW_AUTOSIZE);
-	imshow("corners_window", dst_norm_scaled);
+	namedWindow(source_window, WINDOW_AUTOSIZE);
+	imshow(source_window, copy);
 
 	waitKey(0);
-
 	return(0);
+}
+
+void goodFeaturesToTrack_Demo(int, void*)
+{
+	if (maxCorners < 1) { maxCorners = 1; }
+	vector<Point2f> corners;
+	double qualityLevel = 0.01;
+	double minDistance = 10;
+	int blockSize = 3;
+	bool useHarrisDetector = false;
+	double k = 0.04;
+	Mat copy;
+	copy = src.clone();
+	goodFeaturesToTrack(src_gray,
+		corners,
+		maxCorners,
+		qualityLevel,
+		minDistance,
+		Mat(),
+		blockSize,
+		useHarrisDetector,
+		k);
+	cout << "** Number of corners detected: " << corners.size() << endl;
+	int r = 4;
+	for (size_t i = 0; i < corners.size(); i++)
+	{
+		circle(copy, corners[i], r, Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)), -1, 8, 0);
+	}
+	namedWindow(source_window, WINDOW_AUTOSIZE);
+	imshow(source_window, copy);
 }
