e78eebedd16a6e30fc985df63743408b4f8ee12e
danielhogg <danhogg17@gmail.com>
2017-04-12 11:48:05 -0400

    test commit

--- mvv_demo/core/core.cpp
+++ mvv_demo/core/core.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
 
+#define VERSION "1.0.0"
+#define APPLICATION_NAME "ff"
+
+using namespace std;
 
 int main()
 {
+
+
+	cout << "";
+
+
     return 0;
 }
 
