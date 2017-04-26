c668486f42a651814bb82410bf36b4e45a1a002d
Daniel Hogg <danhogg17@gmail.com>
2017-04-26 13:32:03 -0400

    interpolation trackbar

--- mvv_demo/interpolate_images/interpolate_images.cpp
+++ mvv_demo/interpolate_images/interpolate_images.cpp
@@ -10,35 +10,6 @@
 using namespace cv;
 using namespace std;
 
-static void onChangeTriangleMorph(int morph, void *userdata) //void* mean that it is a pointer of unknown type
-{
-	(*((trackbarTriangleMorph*)userdata)).morph = morph;
-
-	vector<KeyPoint> srcPoints = (*((trackbarTriangleMorph*)userdata)).sourcePoints;
-	vector<KeyPoint> tarPoints = (*((trackbarTriangleMorph*)userdata)).targetPoints;
-	Rect imgSize = (*((trackbarTriangleMorph*)userdata)).imageSize;
-	vector<Point2f> interPoints = construct_intermediate_points(srcPoints, tarPoints, morph);
-	Subdiv2D subdiv = raw_triangulation(interPoints, imgSize);
-	display_triangulation(subdiv, imgSize);
-}
-
-int interpolation_trackbar(vector<Vec6f> trianglesA, vector<Vec6f> trianglesB, Rect imgSizeA, Rect imageSizeB, vector<vector<double>> )
-{
-	trackbarTriangleMorph holder;
-	holder.sourcePoints = sourcePoints;
-	holder.targetPoints = targetPoints;
-	holder.imageSize = imgSize;
-	holder.morph = 0;
-
-	int morph = 0;
-
-	namedWindow("Adjust Window");
-	cvCreateTrackbar2("Morph", "Adjust Window", &morph, 100, onChangeTriangleMorph, (void*)(&holder));
-	waitKey(0);
-
-	return 0;
-}
-
 Mat affine_transform(Vec6f sourceTri, Vec6f targetTri) {
 	cv::Point2f sourceP[3];
 	sourceP[0] = cv::Point2f(sourceTri[0], sourceTri[1]);
@@ -104,4 +75,92 @@ vector<vector<double>> interpolation_preprocessing(vector<Vec6f> sourceT, vector
 	}
 
 	return interpolation_params;
+}
+
+vector<Vec6f> get_interpolated_triangles(vector<Vec6f> sourceT, vector<Vec6f> targetT, vector<vector<double>> affine, int tInt) {
+	int numTriangles = sourceT.size();
+	float t = (float)tInt / 100;
+	vector<Vec6f> interT = vector<Vec6f>();
+
+	for (int i = 0; i < numTriangles; i++) {
+		vector<double> affineParams = affine[i];
+		float pt1x = (1 - t + affineParams[0] * t) * sourceT[i][0] + affineParams[1] * sourceT[i][1] + affineParams[2];
+		float pt1y = (affineParams[3] * t) * sourceT[i][0] + affineParams[4] * sourceT[i][1] + affineParams[5];
+		float pt2x = (1 - t + affineParams[0] * t) * sourceT[i][2] + affineParams[1] * sourceT[i][3] + affineParams[2];
+		float pt2y = (affineParams[3] * t) * sourceT[i][2] + affineParams[4] * sourceT[i][3] + affineParams[5];
+		float pt3x = (1 - t + affineParams[0] * t) * sourceT[i][4] + affineParams[1] * sourceT[i][5] + affineParams[2];
+		float pt3y = (affineParams[3] * t) * sourceT[i][4] + affineParams[4] * sourceT[i][5] + affineParams[5];
+		interT.push_back(Vec6f(pt1x, pt1y, pt2x, pt2y, pt3x, pt3y));
+	}
+	return interT;
+}
+
+void display_interpolated_triangles(vector<Vec6f> triangles, Rect imageBounds) {
+	// the graphical_triangulation function is far too slow
+
+	Scalar active_facet_color(0, 0, 255), delaunay_color(255, 255, 255);
+	Mat img(imageBounds.size(), CV_8UC3);
+
+	img = Scalar::all(0);
+	string win = "Delaunay Demo";
+
+	int numTriangles = triangles.size();
+	for (size_t i = 0; i < numTriangles; i++)
+	{
+		Vec6f t = triangles[i];
+		Point pt0 = Point(cvRound(t[0]), cvRound(t[1]));
+		Point pt1 = Point(cvRound(t[2]), cvRound(t[3]));
+		Point pt2 = Point(cvRound(t[4]), cvRound(t[5]));
+		line(img, pt0, pt1, active_facet_color, 1, LINE_AA, 0);
+		line(img, pt1, pt2, active_facet_color, 1, LINE_AA, 0);
+		line(img, pt2, pt0, active_facet_color, 1, LINE_AA, 0);
+	}
+
+	imshow(win, img);
+	waitKey(1);
+}
+
+struct interpolationMorph {
+	Rect imageSize;
+	int tInt;
+	vector<Vec6f> sourceT;
+	vector<Vec6f> targetT;
+	vector<vector<double>> affineParams;
+};
+
+static void onInterpolate(int tInt, void *userdata) //void* mean that it is a pointer of unknown type
+{
+	(*((interpolationMorph*)userdata)).tInt = tInt;
+
+	vector<Vec6f> sourceT = (*((interpolationMorph*)userdata)).sourceT;
+	vector<Vec6f> targetT = (*((interpolationMorph*)userdata)).targetT;
+	vector<vector<double>> affineParams = (*((interpolationMorph*)userdata)).affineParams;
+	Rect imgSize = (*((interpolationMorph*)userdata)).imageSize;
+	vector<Vec6f> interT = get_interpolated_triangles(sourceT, targetT, affineParams, tInt);
+	display_interpolated_triangles(interT, imgSize);
+	//Subdiv2D subdiv = raw_triangulation(interPoints, imgSize);
+	//display_triangulation(subdiv, imgSize);
+}
+
+int interpolation_trackbar(vector<Vec6f> trianglesA, vector<Vec6f> trianglesB, Rect imgSizeA, Rect imgSizeB, vector<vector<double>> affine)
+{
+	// max of height and weidth
+	int maxWidth = max(imgSizeA.width, imgSizeB.width);
+	int maxHeight = max(imgSizeA.height, imgSizeB.height);
+	Rect imgSize = Rect(0,0,maxWidth, maxHeight);
+
+	interpolationMorph holder;
+	holder.sourceT = trianglesA;
+	holder.targetT = trianglesB;
+	holder.imageSize = imgSize;
+	holder.affineParams = affine;
+	holder.tInt = 0;
+
+	int tInt = 0;
+
+	namedWindow("Adjust Window");
+	cvCreateTrackbar2("Morph", "Adjust Window", &tInt, 100, onInterpolate, (void*)(&holder));
+	waitKey(0);
+
+	return 0;
 }
