091966c5970496142f290bd36b0f2c9792840d04
danielhogg <danhogg17@gmail.com>
2017-05-12 12:21:23 -0400

    csv

--- mvv_demo/core/mvv_iostream.cpp
+++ mvv_demo/core/mvv_iostream.cpp
@@ -18,6 +18,7 @@ void save_csv(string folder_name, string file_name, float** affine_params, int n
 	ofstream csv_file;
 	string full_path = "../data_store/" + folder_name + "/" + file_name;
 	csv_file.open(full_path);
+	csv_file << num_triangles << "\n";
 	for (int i = 0; i < num_triangles; i++) {
 		// affine parameters (12 values, 6 forward, 6 reverse)
 		int num_affine_params = 12;
@@ -33,10 +34,32 @@ float** read_csv(string folder_name, string file_name) {
 	// will read entire csv file
 	// csv[triangle#][12floats]
 
+	int length = 300;
+	float** params = new float*[length];
 
-	// open file
-	float** test = 0;
-	return test;
+	string full_path = "../data_store/" + folder_name + "/" + file_name;
+
+	ifstream csv_file;
+	csv_file.open(full_path);//open the input file
+
+	stringstream str_stream;
+	str_stream << csv_file.rdbuf();//read the file
+	//string str = strStream.str();//str holds the content of the file
+
+	int triangles;
+	string line;
+
+	std::getline(str_stream, line) >> triangles;
+
+
+	while (std::getline(str_stream, line)) {
+		std::cout << line << std::endl;
+	}
+	// conversion between C-style character array and float**
+
+
+	// 
+	return params;
 }
 
 void save_frame(string folder_name, string file_name, char** img) {
@@ -73,8 +96,19 @@ int io_test() {
 			floats[i][j] = (float)i * 0.3425 * (float)j;
 		}
 	}
-
 	save_csv("csv", "triangles.csv", floats, num_triangles);
+
+	//read_csv("csv", "triangles.csv");
+	/*
+	float** result = read_csv("csv", "triangles.csv");
+
+	for (int i = 0; i < 300; i++) {
+		for (int j = 0; j < 12; j++) {
+			cout << result[i][j] << endl;
+		}
+		cout << "\n";
+	}
+	*/
 
 	return 0;
 }
