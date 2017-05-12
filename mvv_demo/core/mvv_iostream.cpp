c456f625212fa4820121dda8a607e22c5c8158b7
Daniel Hogg <danhogg17@gmail.com>
2017-05-12 10:08:16 -0400

    io

--- mvv_demo/core/mvv_iostream.cpp
+++ mvv_demo/core/mvv_iostream.cpp
@@ -13,39 +13,31 @@ using namespace cv;
 
 #define ROOT_DATA_FOLDER "../data_store"
 
-int io_test() {
-	// header is 64 bytes
-
-	
-	auto t1 = std::chrono::high_resolution_clock::now();
-
-
-	string imagePathA = "david_1";
-	string imagePathB = "david_2";
-	string rootPath = "../data_store";
-
-
-	auto t3 = std::chrono::high_resolution_clock::now();
-	cout << "deleting"
-		<< std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t1).count()
-		<< " milliseconds\n";
-
-
-	return 0;
-}
-
 // what IO functions should we have:
 
 void save_csv(string folder_name, string file_name, float** affine_params) {
+	for (int i = 0; i < 300; i++) {
+		for (int j = 0; j < 12; j++) {
+			if (j != 11) {
+				cout << affine_params[i][j] << ",";
+			}
+			else {
+				cout << affine_params[i][j];
+			}
 
+		}
+		cout << "\n";
+	}
 }
 
 float** read_csv(string folder_name, string file_name) {
 	// will read entire csv file
 	// csv[triangle#][12floats]
-	
+
 
 	// open file
+	float** test = 0;
+	return test;
 }
 
 void save_frame(string folder_name, string file_name, char** img) {
@@ -55,6 +47,10 @@ void save_frame(string folder_name, string file_name, char** img) {
 char** read_frame(string folder_name, string file_name) {
 	// we will need a way to quickly read out an image to a char[][]
 	// for cuda processing
+
+	// use imageMagick
+	char** test = 0;
+	return test;
 }
 
 void save_frame_t(string folder_name, string file_name, short** frame_triangle_grid) {
@@ -65,3 +61,21 @@ short** read_frame_t(string folder_name, string file_name) {
 	short** frame = 0;
 	return frame;
 }
+
+int io_test() {
+	// header is 64 bytes
+
+	
+	float** floats = new float*[300];
+	for (int i = 0; i < 300; i++) {
+		floats[i] = new float[12];
+		for (int j = 0; j < 12; j++) {
+			floats[i][j] = (float)i * 0.3425 * (float)j;
+		}
+	}
+
+	save_csv("", "", floats);
+
+	return 0;
+}
+
