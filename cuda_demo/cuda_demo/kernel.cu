3b46666ba650544ccde522dbf4268505cfca7072
szat <artabalt@gmail.com>
2017-05-16 15:29:04 -0400

    cuda simple affine transforms

--- cuda_demo/cuda_demo/kernel.cu
+++ cuda_demo/cuda_demo/kernel.cu
@@ -16,33 +16,6 @@ using namespace cv;
 
 typedef std::chrono::high_resolution_clock Clock;
 
-//load 2d array
-//do something on 2d array
-//send back the 2d array
-
-__global__ void image_to_inter_1C(unsigned char * d_matrix1D_out, unsigned char * d_matrix1D_in, int numRows, int numCols, float tau) {
-	//int rowIdx = blockIdx.x; //give a row per block ==> numRows
-	//int colIdx = threadIdx.x; //give a col per thread, thus a pixel per thread ==> numCols
-	//if (rowIdx < numRows && colIdx < numCols && 0 <= tau && tau <= 1) {
-		//int idx2D_in[] = { rowIdx, colIdx };
-		//int matIdx_in = flatten_idx_gpu(idx2D_in, numRows, numCols);
-		//d_matrix1D_out[matIdx_in] = d_matrix1D_in[matIdx_in];
-		//d_matrix1D_out[matIdx_in] = (unsigned char) (tau * (short) d_matrix1D_in[matIdx_in]);
-		/*
-		int matIdx_in = flatten_idx_gpu(idx2D_in, numRows, numCols);
-		int triangleIdx = d_matrixTriangles1D_in[matIdx_in];
-		if (triangleIdx != -1) {
-			float triangleData[6];
-			for (int i = 0; i < 6; i++) triangleData[i] = d_triangleData1D_in[triangleIdx + i];
-			//using triangleData project pixel in d_matrix1D_in at position matIdx_in onto pixel in d_matrix1D_out at position matIdx_out
-			int idx2D_out[] = { (int)rowIdx*tau, (int)colIdx*tau };
-			int matIdx_out = flatten_idx_gpu(idx2D_out, numRows, numCols);
-			d_matrix1D_out[matIdx_out] = d_matrix1D_in[matIdx_in];
-		}
-		*/
-	//}
-}
-
 __global__
 void kernel2D(uchar* d_output, uchar* d_input, int w, int h, float tau)
 {
@@ -53,31 +26,41 @@ void kernel2D(uchar* d_output, uchar* d_input, int w, int h, float tau)
 	if ((r >= h) || (c >= w)) return;
 
 	d_output[i] = d_input[i] / 2;
-	/*
-	d_output[i].x = (uchar) tau * d_input[i].x;    //Compute red
-	d_output[i].y = (uchar) tau * d_input[i].y; //Compute green
-	d_output[i].z = (uchar) tau * d_input[i].z;  //Compute blue
-	d_output[i].w = 255; // Fully 
-	*/
 }
 
 __device__ 
-void affine_transform(float * newCoord, float * oldCoord, float * affineData) {
-	float cNew = oldCoord[0];
-	float rNew = oldCoord[1];
+void affine_push_pt_gpu(float * newCoord, float * oldCoord, float * affineData, float tau) {
+	/*
+	Parametrization should go:
+	
+	A = [	(1-t) + a_00 * t, a_01 * t,
+			a_10 * t		, (1-t) + a_11 * t]
+	
+	B = [t * b_0, t * b_1]
+
+	((1-t) + a_00 * t)x + (a_01 * t)y + t * b_0, (a_10 * t)x + ((1-t) + a_11 * t)y) + t * b_1 
+	*/
+
+	float cNew = ((1 - tau) + affineData[0] * tau) * oldCoord[0] + (affineData[1] * tau) * oldCoord[1] + tau*affineData[4];
+	float rNew = (affineData[2] * tau) * oldCoord[0] + ((1 - tau) + affineData[3] * tau) * oldCoord[1] + tau*affineData[5];
+	
+	//float cNew = oldCoord[0];
+	//float rNew = oldCoord[1];
 	newCoord[0] = cNew;
 	newCoord[1] = rNew;
 }
 
 __global__
-void raster2D(uchar* d_output, uchar* d_input, int w, int h, float tau, short* d_triangleRaster, float * d_triangleData) {
+void push_image_gpu(uchar* d_output, uchar* d_input, int w, int h, float tau, short* d_triangleRaster, float * d_triangleData) {
 	int c = blockIdx.x*blockDim.x + threadIdx.x;
 	int r = blockIdx.y*blockDim.y + threadIdx.y;
-	int i = r * w + c;
+	int idx = r * w + c;
 
 	if ((r >= h) || (c >= w)) return;
 
-	int triangleIdx = d_triangleRaster[i];
+	int triangleIdx = d_triangleRaster[idx];
+	
+	if (triangleIdx < 0) return;
 
 	float affineData[6] = {
 		d_triangleData[triangleIdx],		d_triangleData[triangleIdx + 1],
@@ -85,79 +68,86 @@ void raster2D(uchar* d_output, uchar* d_input, int w, int h, float tau, short* d
 		d_triangleData[triangleIdx + 4],	d_triangleData[triangleIdx + 5]
 	};
 
-	//do operations on c and r (col and row)
-	float oldCoord[2] = { c, r };
+	float oldCoord[2] = { c, r };	
 	float newCoord[2];
 
-	affine_transform(newCoord, oldCoord, affineData);
+	//--The computation part
+	affine_push_pt_gpu(newCoord, oldCoord, affineData, tau);
 	
-	int newC = (int) newCoord[0];
-	int newR = (int) newCoord[1];
-	int newI = newR * w + newC;
+	if (((int)newCoord[1] >= h) || ((int)newCoord[0] >= w) || (int)newCoord[1] < 0 || (int)newCoord[0] < 0) return;
 
-	d_output[newI] = d_input[i] /5;
+	//--Snapping the new coordinate to the pixel
+	int newIdx = newCoord[1] * w + newCoord[0]; //newC = newCoord[0], newR = newCoord[1];
+
+	d_output[newIdx] = d_input[idx] * tau;
 }
 
 int main(int argc, char ** argv) {
 	cout << "Welcome to cuda_demo testing unit!" << endl;
 	cout << "Loading one image with openCV! (grayscale)" << endl;
 
-	string address1 = "..\\data_store\\big_picture.jpg";
+	string address1 = "..\\data_store\\medium_picture.jpg";
 	Mat img1 = imread(address1, IMREAD_GRAYSCALE);
 
 	int triangleNb = 1;
 	int W = img1.size().width;
 	int H = img1.size().height;
 
-	uchar *h_in;
-	uchar *h_out;
+	uchar *h_imgIn;
+	uchar *h_imgOut;
 	
-	h_in = (uchar*)malloc(W*H * sizeof(uchar));
-	h_out = (uchar*)malloc(W*H * sizeof(uchar));
-
+	h_imgIn = (uchar*)malloc(W*H * sizeof(uchar));
 	Mat img1Flat = img1.reshape(1, 1);
-	h_in = img1Flat.data;
+	h_imgIn = img1Flat.data;
 
-	uchar * d_in;
-	uchar * d_out;
-	cudaMalloc((void**)&d_in, W*H * sizeof(uchar));
-	cudaMalloc((void**)&d_out, W*H * sizeof(uchar));
+	h_imgOut = (uchar*)malloc(W*H * sizeof(uchar));
+	for (int j = 0; j < W*H; j++) h_imgOut[j] = 0;
 
-	cudaMemcpy(d_in, h_in, W*H * sizeof(uchar), cudaMemcpyHostToDevice);
-	
+	//--Dummy values, to be replaced later by the actual raster
 	short * h_triangleRaster = new short[W*H];
-	for (int i = 0; i < W*H; i++) h_triangleRaster[i] = 0;
-	float h_triangleData[6] = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
+	for (int j = 0; j < W*H; j++) h_triangleRaster[j] = 0;
 
+	float h_triangleData[6] = { 1, 0.2, 0, 1, 50, 50}; //corresponding to the null transformation
+
+	//--Sending the data to the GPU memory
 	short * d_triangleRaster;
-	float * d_triangleData;
-
 	cudaMalloc((void**)&d_triangleRaster, W*H * sizeof(short));
 	cudaMemcpy(d_triangleRaster, h_triangleRaster, W*H * sizeof(short), cudaMemcpyHostToDevice);
 
+	float * d_triangleData;
 	cudaMalloc((void**)&d_triangleData, 6 * sizeof(float));
 	cudaMemcpy(d_triangleData, h_triangleData, 6 * sizeof(float), cudaMemcpyHostToDevice);
 
-	dim3 blockSize(32, 32);
+	uchar * d_imgIn;
+	cudaMalloc((void**)&d_imgIn, W*H * sizeof(uchar));
+	cudaMemcpy(d_imgIn, h_imgIn, W*H * sizeof(uchar), cudaMemcpyHostToDevice);
 
+	uchar * d_imgOut;
+	cudaMalloc((void**)&d_imgOut, W*H * sizeof(uchar));
+	cudaMemcpy(d_imgOut, h_imgOut, W*H * sizeof(uchar), cudaMemcpyHostToDevice);
+
+	//--GPU variables
+
+	dim3 blockSize(32, 32);
 	int bx = (W + 32 - 1) / 32;
 	int by = (H + 32 - 1) / 32;
 	dim3 gridSize = dim3(bx, by);
 
-
-
 	auto t1 = Clock::now();
-	raster2D << <gridSize, blockSize >> >(d_out, d_in, W, H, 0.3, d_triangleRaster, d_triangleData);	
+	push_image_gpu << <gridSize, blockSize >> >(d_imgOut, d_imgIn, W, H, 1, d_triangleRaster, d_triangleData);	
 	auto t2 = Clock::now();
 	std::cout << "delta time " << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << std::endl;
 
-	cudaMemcpy(h_out, d_out, W*H * sizeof(uchar), cudaMemcpyDeviceToHost);
+	//--Send data back to the host from the GPU and free memory
+	cudaMemcpy(h_imgOut, d_imgOut, W*H * sizeof(uchar), cudaMemcpyDeviceToHost);
 
-	cudaFree(d_in);
-	cudaFree(d_out);
+	cudaFree(d_imgIn);
+	cudaFree(d_imgOut);
+	cudaFree(d_triangleData);
+	cudaFree(h_triangleRaster);
 
-	Mat out = Mat(1, W*H, CV_8UC1, h_out);
-	out = out.reshape(1, H);
+	Mat render1 = Mat(1, W*H, CV_8UC1, h_imgOut);
+	render1 = render1.reshape(1, H);
 
 	return 0;
 }
