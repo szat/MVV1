26872602afeab96e6e8258ae8cb46681960152e0
szat <artabalt@gmail.com>
2017-05-16 12:06:47 -0400

    scafolding done for cuda

--- cuda_demo/cuda_demo/kernel.cu
+++ cuda_demo/cuda_demo/kernel.cu
@@ -20,65 +20,13 @@ typedef std::chrono::high_resolution_clock Clock;
 //do something on 2d array
 //send back the 2d array
 
-__global__ void cube(float * d_out, float * d_in) {
-	int idx = threadIdx.x;
-	float f = d_in[idx];
-	d_out[idx] = f*f*f;
-}
-
-__device__ int flatten_idx_gpu(int * matrixIdx, int numRows, int numCols) {
-	if (0 <= matrixIdx[0] < numRows && 0 <= matrixIdx[1] < numCols) return matrixIdx[0] * numCols + matrixIdx[1];
-	else return 0;
-}
-
-__device__ int * unflatten_idx_gpu(int arrayIdx, int numRows, int numCols) {
-	if (0 <= arrayIdx < numRows*numCols) {
-		int rowIdx = arrayIdx / numCols;
-		int idx[] = { rowIdx ,  arrayIdx - rowIdx*numCols };
-		return idx;
-	}
-	return 0;
-}
-
-__global__ void rowOperation_block(float * d_matrix1D_out, float * d_matrix1D_in, int numRows, int numCols) {
-	//number of blocks should be the number of rows
-	//number of threads should be the number of cols
-	int idxBlk = blockIdx.x;
-	int idxThd = threadIdx.x;
-	//give a row per block, in a 2d matrix it would be mat[idxBlk][idxThd], mat[idxBlk][idxThd], mat[idxBlk][idxThd], ...
-	int idx2D[] = { idxBlk, idxThd };
-	int matIdx = flatten_idx_gpu(idx2D, numRows, numCols);
-	d_matrix1D_out[matIdx] = d_matrix1D_in[matIdx] + 1;
-}
-
-__global__ void colOperation_block(float * d_matrix1D_out, float * d_matrix1D_in, int numRows, int numCols) {
-	int idxBlk = blockIdx.x;
-	int idxThd = threadIdx.x;
-	//give a column per thread, in a 2d matrix it would be mat[idxThd][idxBlk], mat[idxThd][idxBlk], mat[idxThd][idxBlk], ...
-	int idx2D[] = { idxThd, idxBlk };
-	int matIdx = flatten_idx_gpu(idx2D, numRows, numCols);
-	d_matrix1D_out[matIdx] = d_matrix1D_in[matIdx] + 0.01;
-}
-
-__global__ void rowOperation(float * d_matrix1D_out, float * d_matrix1D_in, int numRows, int numCols) {
-	int idx = threadIdx.x;
-	//printf("We are in thread %d\n", idx);
-	//give a row per thread, in a 2d matrix it would be mat[idx][0], mat[idx][1], mat[idx][2], ...
-	for (int col = 0; col < numCols; col++) {
-		int idx2D[] = { idx, col };
-		int matIdx = flatten_idx_gpu(idx2D, numRows, numCols);
-		int matIdx_out = 3 * (matIdx + 32) % numCols;
-		d_matrix1D_out[matIdx_out] = d_matrix1D_in[matIdx] + 1; //in our sample code should increment the rows from 2.14 to 3.14 for instance
-	}
-}
-
 __global__ void image_to_inter_1C(unsigned char * d_matrix1D_out, unsigned char * d_matrix1D_in, int numRows, int numCols, float tau) {
-	int rowIdx = blockIdx.x; //give a row per block ==> numRows
-	int colIdx = threadIdx.x; //give a col per thread, thus a pixel per thread ==> numCols
+	//int rowIdx = blockIdx.x; //give a row per block ==> numRows
+	//int colIdx = threadIdx.x; //give a col per thread, thus a pixel per thread ==> numCols
 	//if (rowIdx < numRows && colIdx < numCols && 0 <= tau && tau <= 1) {
-		int idx2D_in[] = { rowIdx, colIdx };
-		int matIdx_in = flatten_idx_gpu(idx2D_in, numRows, numCols);
-		d_matrix1D_out[matIdx_in] = d_matrix1D_in[matIdx_in];
+		//int idx2D_in[] = { rowIdx, colIdx };
+		//int matIdx_in = flatten_idx_gpu(idx2D_in, numRows, numCols);
+		//d_matrix1D_out[matIdx_in] = d_matrix1D_in[matIdx_in];
 		//d_matrix1D_out[matIdx_in] = (unsigned char) (tau * (short) d_matrix1D_in[matIdx_in]);
 		/*
 		int matIdx_in = flatten_idx_gpu(idx2D_in, numRows, numCols);
@@ -95,38 +43,8 @@ __global__ void image_to_inter_1C(unsigned char * d_matrix1D_out, unsigned char 
 	//}
 }
 
-__global__ void colOperation(float * d_matrix1D_out, float * d_matrix1D_in, int numRows, int numCols) {
-	int idx = threadIdx.x;
-	//give a column per thread, in a 2d matrix it would be mat[0][idx], mat[1][idx], mat[2][idx], ...
-	for (int row = 0; row < numRows; row++) {
-		int idx2D[] = { row, idx };
-		int matIdx = flatten_idx_gpu(idx2D, numRows, numCols);
-		d_matrix1D_out[matIdx] = d_matrix1D_in[matIdx] + 0.01; //in ou sample code should increment the cols from 2.14 to 2.15
-	}
-}
-
-int flatten_idx(int * matrixIdx, int numRows, int numCols) {
-	if (0 <= matrixIdx[0] < numRows && 0 <= matrixIdx[1] < numCols) return matrixIdx[0] * numCols + matrixIdx[1];
-	else return 0;
-}
-
-int * unflatten_idx(int arrayIdx, int numRows, int numCols) {
-	if (0 <= arrayIdx < numRows*numCols) {
-		int rowIdx = arrayIdx / numCols;
-		int idx[] = { rowIdx ,  arrayIdx - rowIdx*numCols };
-		return idx;
-	}
-	return 0;
-}
-
-__global__ void compute2D(uchar4 * imageData_out, uchar4 * imageData_in, float tau) {
-	int c = blockIdx.x*blockDim.x + threadIdx.x;
-	int r = blockIdx.y*blockDim.y + threadIdx.y;
-
-}
-
 __global__
-void kernel2D(uchar *d_output, uchar* d_input, int w, int h, float tau)
+void kernel2D(uchar* d_output, uchar* d_input, int w, int h, float tau)
 {
 	int c = blockIdx.x*blockDim.x + threadIdx.x;
 	int r = blockIdx.y*blockDim.y + threadIdx.y;
@@ -143,38 +61,97 @@ void kernel2D(uchar *d_output, uchar* d_input, int w, int h, float tau)
 	*/
 }
 
+__device__ 
+void affine_transform(float * newCoord, float * oldCoord, float * affineData) {
+	float cNew = oldCoord[0];
+	float rNew = oldCoord[1];
+	newCoord[0] = cNew;
+	newCoord[1] = rNew;
+}
+
+__global__
+void raster2D(uchar* d_output, uchar* d_input, int w, int h, float tau, short* d_triangleRaster, float * d_triangleData) {
+	int c = blockIdx.x*blockDim.x + threadIdx.x;
+	int r = blockIdx.y*blockDim.y + threadIdx.y;
+	int i = r * w + c;
+
+	if ((r >= h) || (c >= w)) return;
+
+	int triangleIdx = d_triangleRaster[i];
+
+	float affineData[6] = {
+		d_triangleData[triangleIdx],		d_triangleData[triangleIdx + 1],
+		d_triangleData[triangleIdx + 2],	d_triangleData[triangleIdx + 3],
+		d_triangleData[triangleIdx + 4],	d_triangleData[triangleIdx + 5]
+	};
+
+	//do operations on c and r (col and row)
+	float oldCoord[2] = { c, r };
+	float newCoord[2];
+
+	affine_transform(newCoord, oldCoord, affineData);
+	
+	int newC = (int) newCoord[0];
+	int newR = (int) newCoord[1];
+	int newI = newR * w + newC;
+
+	d_output[newI] = d_input[i] /5;
+}
+
 int main(int argc, char ** argv) {
 	cout << "Welcome to cuda_demo testing unit!" << endl;
 	cout << "Loading one image with openCV! (grayscale)" << endl;
 
 	string address1 = "..\\data_store\\big_picture.jpg";
 	Mat img1 = imread(address1, IMREAD_GRAYSCALE);
-	const int W = img1.size().width;
-	const int H = img1.size().height;
-	const int ARRAY_BYTES = W*H * sizeof(uchar);
+
+	int triangleNb = 1;
+	int W = img1.size().width;
+	int H = img1.size().height;
 
 	uchar *h_in;
 	uchar *h_out;
-	h_in = (uchar*)malloc(ARRAY_BYTES);
-	h_out = (uchar*)malloc(ARRAY_BYTES);
+	
+	h_in = (uchar*)malloc(W*H * sizeof(uchar));
+	h_out = (uchar*)malloc(W*H * sizeof(uchar));
 
 	Mat img1Flat = img1.reshape(1, 1);
 	h_in = img1Flat.data;
 
 	uchar * d_in;
 	uchar * d_out;
-	cudaMalloc((void**)&d_in, ARRAY_BYTES);
-	cudaMalloc((void**)&d_out, ARRAY_BYTES);
+	cudaMalloc((void**)&d_in, W*H * sizeof(uchar));
+	cudaMalloc((void**)&d_out, W*H * sizeof(uchar));
 
-	const dim3 blockSize(32, 32);
+	cudaMemcpy(d_in, h_in, W*H * sizeof(uchar), cudaMemcpyHostToDevice);
+	
+	short * h_triangleRaster = new short[W*H];
+	for (int i = 0; i < W*H; i++) h_triangleRaster[i] = 0;
+	float h_triangleData[6] = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
 
-	const int bx = (W + 32 - 1) / 32;
-	const int by = (H + 32 - 1) / 32;
-	const dim3 gridSize = dim3(bx, by);
+	short * d_triangleRaster;
+	float * d_triangleData;
 
-	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
-	kernel2D << <gridSize, blockSize >> >(d_out, d_in, W, H, 0.3);	
-	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+	cudaMalloc((void**)&d_triangleRaster, W*H * sizeof(short));
+	cudaMemcpy(d_triangleRaster, h_triangleRaster, W*H * sizeof(short), cudaMemcpyHostToDevice);
+
+	cudaMalloc((void**)&d_triangleData, 6 * sizeof(float));
+	cudaMemcpy(d_triangleData, h_triangleData, 6 * sizeof(float), cudaMemcpyHostToDevice);
+
+	dim3 blockSize(32, 32);
+
+	int bx = (W + 32 - 1) / 32;
+	int by = (H + 32 - 1) / 32;
+	dim3 gridSize = dim3(bx, by);
+
+
+
+	auto t1 = Clock::now();
+	raster2D << <gridSize, blockSize >> >(d_out, d_in, W, H, 0.3, d_triangleRaster, d_triangleData);	
+	auto t2 = Clock::now();
+	std::cout << "delta time " << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << std::endl;
+
+	cudaMemcpy(h_out, d_out, W*H * sizeof(uchar), cudaMemcpyDeviceToHost);
 
 	cudaFree(d_in);
 	cudaFree(d_out);
@@ -182,82 +159,5 @@ int main(int argc, char ** argv) {
 	Mat out = Mat(1, W*H, CV_8UC1, h_out);
 	out = out.reshape(1, H);
 
-
 	return 0;
-	/*
-
-
-	const int W = img1.size().width;
-	const int H = img1.size().height;
-
-	dim3 blockSize(32, 32); 
-	
-	int bx = (W + blockSize.x - 1) / blockSize.x;
-	int by = (H + blockSize.y - 1) / blockSize.y;
-	dim3 gridSize = dim3(bx, by);
-
-	const int numRows = img1.rows;
-	const int numCols = img1.cols;
-
-	const int ARRAY_SIZE = numRows * numCols;
-	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(unsigned char);
-
-	unsigned char *h_img1Data_in;
-	unsigned char *h_img1Data_out;
-	h_img1Data_out = (unsigned char *)malloc(ARRAY_BYTES);
-	h_img1Data_in = (unsigned char *)malloc(ARRAY_BYTES);
-	
-	Mat img1Flat = img1.reshape(1, 1);
-	h_img1Data_in = img1Flat.data;
-
-	//for(int i = 0; i < ARRAY_SIZE; i++) h_img1Data_out[i] = 0; //just to put something in it
-
-	unsigned char * d_img1Data_in;
-	unsigned char * d_img1Data_out;
-
-	cudaMalloc((void**)&d_img1Data_in, ARRAY_BYTES);
-	cudaMalloc((void**)&d_img1Data_out, ARRAY_BYTES);
-
-	cout << "number of bytes " << ARRAY_BYTES << endl;
-
-	for (int i = 0; i < 1; i++) {
-		cudaMemcpy(d_img1Data_in, h_img1Data_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
-		auto t1 = Clock::now();
-		image_to_inter_1C << <numRows, numCols >> > (d_img1Data_out, d_img1Data_in, numRows, numCols, 0.7);
-		cudaMemcpy(h_img1Data_out, d_img1Data_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
-		auto t2 = Clock::now();
-		std::cout << "delta time " << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << std::endl;
-	}
-
-	Mat out = Mat(1, numRows*numCols, CV_8UC1, h_img1Data_out);
-
-	cout << "out size rows " << out.rows << endl;
-	cout << "out size cols " << out.cols << endl;
-
-	cout << "out size height " << out.size().height << endl;
-	cout << "out size width " << out.size().width << endl;
-
-	out = out.reshape(1, numRows);
-
-	cout << "out size rows " << out.rows << endl;
-	cout << "out size cols " << out.cols << endl;
-
-	cout << "out size height " << out.size().height << endl;
-	cout << "out size width " << out.size().width << endl;
-
-	namedWindow("Result", WINDOW_AUTOSIZE);
-	imshow("Result", out);
-	waitKey(0);
-	cin.ignore();
-
-	//cudaThreadSynchronize();
-
-	//Free
-	cudaFree(d_img1Data_in);
-	cudaFree(d_img1Data_out);
-
-	
-
-	return 0;
-	*/
 }
