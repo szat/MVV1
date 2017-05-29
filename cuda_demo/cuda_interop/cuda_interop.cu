53fbd11675cc1eed7d74ec065b0ae64c1165b1d6
danielhogg <danhogg17@gmail.com>
2017-05-29 13:13:23 -0400

    working interpolate

--- cuda_demo/cuda_interop/cuda_interop.cu
+++ cuda_demo/cuda_interop/cuda_interop.cu
@@ -60,6 +60,66 @@ cudaGraphicsResource *resource;
 __device__ int counter;
 __device__ volatile int param = 50;
 
+__global__
+void kernel2D_subpix(uchar4* d_output, uchar4* d_input, short* d_raster1, int w, int h, float * d_affineData, int subDiv, float tau, bool reverse)
+{
+	if (tau > 1 || tau < 0) return;
+
+	int col = blockIdx.x*blockDim.x + threadIdx.x;
+	int row = blockIdx.y*blockDim.y + threadIdx.y;
+	int raster_index = (row * w + col);
+	//int color_index = raster_index * 3;
+
+	// should not need to do this check if everything is good, must be an extra pixel
+	if (raster_index >= w * h) return;
+	if ((row >= h) || (col >= w)) return;
+
+	short affine_index = d_raster1[raster_index];
+	short offset = (affine_index - 1) * 12;
+	if (reverse) {
+		offset += 6;
+	}
+	if (affine_index != 0) {
+		// triangle indexes start at 1
+		float diff = 1 / (float)subDiv;
+		for (int i = 0; i < subDiv; i++) {
+			for (int j = 0; j < subDiv; j++) {
+				int new_c = (int)(((1 - tau) + tau*d_affineData[offset]) * (float)(col - 0.5 + (diff * i)) + (tau * d_affineData[offset + 1]) * (float)(row - 0.5 + (diff * j)) + (tau * d_affineData[offset + 2]));
+				int new_r = (int)((tau * d_affineData[offset + 3]) * (float)(col - 0.5 + (diff * i)) + ((1 - tau) + tau * d_affineData[offset + 4]) * (float)(row - 0.5 + (diff * j)) + (tau * d_affineData[offset + 5]));
+				if ((new_r >= h) || (new_c >= w) || (new_r < 0) || (new_c < 0)) return;
+				int new_i = new_r * w + new_c;
+				d_output[new_i] = d_input[raster_index];
+			}
+		}
+	}
+
+
+}
+
+__global__
+void kernel2D_add(uchar4* d_output, uchar4* d_input_1, uchar4* d_input_2, int w, int h, float tau) {
+	//tau is from a to b
+	int col = blockIdx.x*blockDim.x + threadIdx.x;
+	int row = blockIdx.y*blockDim.y + threadIdx.y;
+	int raster_index = (row * w + col);
+
+	// should not need to do this check if everything is good, must be an extra pixel
+	if (raster_index >= w * h) return;
+	if ((row >= h) || (col >= w)) return;
+
+
+	if (d_input_1[raster_index].x == 0 && d_input_1[raster_index].y == 0 && d_input_1[raster_index].z == 0) {
+		d_output[raster_index] = d_input_2[raster_index];
+	}
+	else if (d_input_2[raster_index].x == 0 && d_input_2[raster_index].y == 0 && d_input_2[raster_index].z == 0) {
+		d_output[raster_index] = d_input_1[raster_index];
+	}
+	else {
+		d_output[raster_index].x = tau*d_input_1[raster_index].x + (1 - tau)*d_input_2[raster_index].x;
+		d_output[raster_index].y = tau*d_input_1[raster_index].y + (1 - tau)*d_input_2[raster_index].y;
+		d_output[raster_index].z = tau*d_input_1[raster_index].z + (1 - tau)*d_input_2[raster_index].z;
+	}
+}
 
 __global__ void shift_image(uchar4 *ptr, uchar4* d_img_ptr, int w, int h, int param) {
 	// map from threadIdx/BlockIdx to pixel position
@@ -221,30 +281,111 @@ int main(int argc, char **argv)
 	*/
 	int morphing_param = 0;
 	for (;;) {
-		
+		auto t1 = std::chrono::high_resolution_clock::now();
 
 		string img_path_1 = "../../data_store/binary/david_1.bin";
 		string img_path_2 = "../../data_store/binary/david_2.bin";
+		string raster1_path = "../../data_store/raster/rasterA.bin";
+		string raster2_path = "../../data_store/raster/rasterB.bin";
+		string affine_path = "../../data_store/affine/affine_1.bin";
 
+		// BINARY IMAGE READ
 		int length_1 = 0;
-		int width_1 = 0;
-		int height_1 = 0;
 		int length_2 = 0;
+		int width_1 = 0;
 		int width_2 = 0;
+		int height_1 = 0;
 		int height_2 = 0;
-		uchar4* h_onload_ptr_1 = read_uchar4_array(img_path_1, length_1, width_1, height_1);
-		uchar4* h_onload_ptr_2 = read_uchar4_array(img_path_2, length_2, width_2, height_2);
+		uchar4 *h_in_1 = read_uchar4_array(img_path_1, length_1, width_1, height_1);
+		uchar4 *h_in_2 = read_uchar4_array(img_path_2, length_2, width_2, height_2);
 
-		uchar4* d_onload_ptr_1;
-		uchar4* d_onload_ptr_2;
 
-		cudaMalloc((void**)&d_onload_ptr_1, memsize);
-		cudaMalloc((void**)&d_onload_ptr_2, memsize);
-		cudaMemcpy(d_onload_ptr_1, h_onload_ptr_1, memsize, cudaMemcpyHostToDevice);
-		cudaMemcpy(d_onload_ptr_2, h_onload_ptr_2, memsize, cudaMemcpyHostToDevice);
+		// RASTER READ
+		int num_pixels_1 = 0;
+		int num_pixels_2 = 0;
+		short *h_raster1 = read_short_array(raster1_path, num_pixels_1);
+		short *h_raster2 = read_short_array(raster2_path, num_pixels_2);
 
-		cudaMemcpy(d_render_final, h_onload_ptr_1, memsize, cudaMemcpyHostToDevice);
+		// AFFINE READ
+		int num_floats = 0;
+		float *h_affine_data = read_float_array(affine_path, num_floats);
+		int num_triangles = num_floats / 12;
 
+		if (height_1 != height_2 || width_1 != width_2) {
+			cout << "Incompatible image sizes. Program will now crash.\n";
+			exit(-1);
+		}
+
+		uchar4 *h_out_1;
+		uchar4 *h_out_2;
+		uchar4 *h_sum;
+
+		// there must be a faster way to initialize these arrays to all zeros
+		h_out_1 = new uchar4[width * height];
+		h_out_2 = new uchar4[width * height];
+		h_sum = new uchar4[width * height];
+
+		//--Sending the data to the GPU memory
+		cout << "declaring device data-structures..." << endl;
+
+		float * d_affine_data;
+		cudaMalloc((void**)&d_affine_data, num_floats * sizeof(float));
+		cudaMemcpy(d_affine_data, h_affine_data, num_floats * sizeof(float), cudaMemcpyHostToDevice);
+
+		short *d_raster1;
+		cudaMalloc((void**)&d_raster1, width * height * sizeof(short));
+		cudaMemcpy(d_raster1, h_raster1, width * height * sizeof(short), cudaMemcpyHostToDevice);
+
+		short *d_raster2;
+		cudaMalloc((void**)&d_raster2, width * height * sizeof(short));
+		cudaMemcpy(d_raster2, h_raster2, width * height * sizeof(short), cudaMemcpyHostToDevice);
+
+		uchar4 * d_in_1;
+		cudaMalloc((void**)&d_in_1, memsize);
+		cudaMemcpy(d_in_1, h_in_1, memsize, cudaMemcpyHostToDevice);
+
+		uchar4 * d_out_1;
+		cudaMalloc((void**)&d_out_1, memsize);
+		cudaMemcpy(d_out_1, h_out_1, memsize, cudaMemcpyHostToDevice);
+
+		uchar4 * d_in_2;
+		cudaMalloc((void**)&d_in_2, memsize);
+		cudaMemcpy(d_in_2, h_in_2, memsize, cudaMemcpyHostToDevice);
+
+		uchar4 * d_out_2;
+		cudaMalloc((void**)&d_out_2, memsize);
+		cudaMemcpy(d_out_2, h_out_2, memsize, cudaMemcpyHostToDevice);
+
+		uchar4 * d_sum;
+		cudaMalloc((void**)&d_sum, memsize);
+		cudaMemcpy(d_sum, h_sum, memsize, cudaMemcpyHostToDevice);
+
+		float tau = (float)(morphing_param % 200) * 0.005f;
+
+		float reverse_tau = 1.0f - tau;
+		int reversal_offset = 0;
+
+		kernel2D_subpix << <gridSize, blockSize >> >(d_out_1, d_in_1, d_raster1, width, height, d_affine_data, 4, tau, false);
+		kernel2D_subpix << <gridSize, blockSize >> >(d_out_2, d_in_2, d_raster2, width, height, d_affine_data, 4, reverse_tau, true);
+		kernel2D_add << <gridSize, blockSize >> > (d_sum, d_out_1, d_out_2, width, height, tau);
+
+		cudaMemcpy(h_out_1, d_out_1, memsize, cudaMemcpyDeviceToHost);
+		cudaMemcpy(h_out_2, d_out_2, memsize, cudaMemcpyDeviceToHost);
+		cudaMemcpy(h_sum, d_sum, memsize, cudaMemcpyDeviceToHost);
+
+		cudaFree(d_in_1);
+		cudaFree(d_out_1);
+		cudaFree(d_raster1);
+		cudaFree(d_in_2);
+		cudaFree(d_out_2);
+		cudaFree(d_raster2);
+		cudaFree(d_affine_data);
+
+
+		auto t2 = std::chrono::high_resolution_clock::now();
+		std::cout << "write short took "
+			<< std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
+			<< " milliseconds\n";
 
 		dim3 blockSize(32, 32);
 		int bx = (width + 32 - 1) / 32;
@@ -256,15 +397,16 @@ int main(int argc, char **argv)
 		uchar4* d_render_final;
 		size_t  size;
 
-		cudaGraphicsResourceGetMappedPointer((void**)&d_render_final, &size, resource);
 
+
+		cudaGraphicsResourceGetMappedPointer((void**)&d_render_final, &size, resource);
+		cudaMemcpy(d_render_final, d_sum, memsize, cudaMemcpyDeviceToDevice);
 		flip_y << < gridSize, blockSize >> >(d_render_final, width, height);
-		morph_image << <gridSize, blockSize >> >(d_render_final, width, height, morphing_param);
 
 		cudaGraphicsUnmapResources(1, &resource, NULL);
 
-		free(h_onload_ptr_1);
-		free(h_onload_ptr_2);
+		cudaFree(d_sum);
+		cudaFree(d_render_final);
 		morphing_param++;
 		//Does not seem "necessary"
 		cudaDeviceSynchronize();
