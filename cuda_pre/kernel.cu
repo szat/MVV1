97c849c4dca286346c984d43823d795b0b1502dc
szat <artabalt@gmail.com>
2017-06-10 21:08:54 -0400

    spx sizes and centers

--- cuda_pre/kernel.cu
+++ cuda_pre/kernel.cu
@@ -11,10 +11,28 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <queue>
 
 using namespace std;
 using namespace cv;
 using namespace cv::ximgproc;
+
+__global__
+void slic_segmentation(uchar3* cielab, int* labels, int width, int height, int spx_nb, int compactness, int metric) {
+	int col = blockIdx.x*blockDim.x + threadIdx.x;
+	int row = blockIdx.y*blockDim.y + threadIdx.y;
+	int i = (row * width + col);
+
+	// should not need to do this check if everything is good, must be an extra pixel
+	if (i >= width * height) return;
+	if ((row >= height) || (col >= width)) return;
+
+	uchar3 blank = uchar3();
+	blank.x = 0;
+	blank.y = 0;
+	blank.z = 0;
+	cielab[i] = blank;
+}
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
@@ -38,6 +56,164 @@ Mat visualize_labels(Mat labels) {
 	return label_viz;
 }
 
+typedef vector<Point> Contour;
+
+void get_spx_data(Mat & labels_in, int spx_nb_in, vector<int> & sizes_out, vector<Point> & centers_out, vector<Contour> & contours_out) {
+	//Stupid init
+
+	for (int i = 0; i < spx_nb_in; i++) {
+		int nb = 0;
+		sizes_out.push_back(nb);
+
+		Point center;
+		center.x = 0;
+		center.y = 0;
+		centers_out.push_back(center);
+		
+		Contour contour;
+		contours_out.push_back(contour);
+	}
+
+	//A center is an average of the positions of the pixels for a label
+	for (int i = 0; i < labels_in.rows; i++) {
+		for (int j = 0; j < labels_in.cols; j++) {
+			int id = labels_in.at<int>(i, j);
+			sizes_out.at(id)++;
+			//This is correct, double checked
+			centers_out.at(id).x += j;
+			centers_out.at(id).y += i;
+		}
+	}
+
+	for (int i = 0; i < spx_nb_in; i++) {
+		centers_out.at(i).x = centers_out.at(i).x / sizes_out.at(i);
+		centers_out.at(i).y = centers_out.at(i).y / sizes_out.at(i);
+	}
+
+	/*
+	for (int i = 0; i < spx_nb_in; i++) {
+		int col = centers_out.at(i).x;
+		int row = centers_out.at(i).y;
+		if (i != labels_in.at<int>(row, col)) {
+			cout << "spx " << i << " has center at (" << row << " , " << col << ")" << endl;
+		}
+	}
+	*/
+
+
+	//if a center is not in the spx it represents, find a near representative
+	for (int i = 0; i < spx_nb_in; i++) {
+		int col = centers_out.at(i).x;
+		int row = centers_out.at(i).y;
+		if (i != labels_in.at<int>(row, col)) {
+			Point new_center;
+			int ray = (int)sqrt((float)sizes_out.at(i));
+			int rows = labels_in.rows;
+			int cols = labels_in.cols;
+
+			for (int j = 0; j < ray; j++) {
+				int new_row = row - j;
+				int new_col = col - j;			
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+
+				new_row = row;
+				new_col = col - j;
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+				new_row = row + 1;
+				new_col = col - j;
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+
+				new_row = row - 1;
+				new_col = col;
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+				new_row = row + 1;
+				new_col = col;
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+
+				new_row = row - 1;
+				new_col = col + 1;
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+
+				new_row = row;
+				new_col = col + 1;
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+
+				new_row = row + 1;
+				new_col = col + 1;
+				if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols) {
+					if (i == labels_in.at<int>(new_row, new_col)) {
+						new_center.x = new_col;
+						new_center.y = new_row;
+						centers_out.at(i) = new_center;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	/*
+	for (int i = 0; i < spx_nb_in; i++) {
+		int col = centers_out.at(i).x;
+		int row = centers_out.at(i).y;
+		if (i != labels_in.at<int>(row, col)) {
+			cout << "spx " << i << " has center at (" << row << " , " << col << ")" << endl;
+		}
+	}
+	*/
+
+	//Finding Contours
+}
+
 int main()
 {
 	//Getting the label mask from segmentation
@@ -50,7 +226,7 @@ int main()
 		cout << "Could not open image..." << img_file << "\n";
 		return -1;
 	}
-
+	
 	Mat converted;
 	cvtColor(img, converted, COLOR_BGR2HSV);
 
@@ -58,7 +234,7 @@ int main()
 	int region_size = 25;
 	int ruler = 45;
 	int min_element_size = 50;
-	int num_iterations = 5;
+	int num_iterations = 6;
 
 	cout << "New computation!" << endl;
 
@@ -75,119 +251,36 @@ int main()
 	Mat labels;
 	slic->getLabels(labels);
 
-	//	Mat label_viz = visualize_labels(labels);
+	Mat only_contour = labels.clone();
+	for (int i = 1; i < only_contour.rows-1; i++) {
+		for (int j = 1; j < only_contour.cols-1; j++) {
+			int id = only_contour.at<int>(i, j);
+			if (labels.at<int>(i - 1, j - 1) != id || labels.at<int>(i - 1, j) != id || labels.at<int>(i - 1, j + 1) != id ||
+				labels.at<int>(i, j - 1) != id										 || labels.at<int>(i, j + 1) != id ||
+				labels.at<int>(i + 1, j - 1) != id || labels.at<int>(i + 1, j) != id || labels.at<int>(i + 1, j + 1) != id)
+			{	
+				only_contour.at<int>(i, j) = 0;
+			}
+		}
+	}
+
+	Mat label_viz = visualize_labels(labels);
+	Mat contour_viz = visualize_labels(only_contour);
+		
+	int spx_nb = slic->getNumberOfSuperpixels();
+
+	vector<Contour> contours_out;
+	vector<Point> centers_out;
+	vector<int> spx_sizes_out;
+	get_spx_data(labels, spx_nb, spx_sizes_out, centers_out, contours_out);
+	
+	circle(result, centers_out.at(853), 2, Scalar(0, 0, 255), 2,0);
+
 
 	int width = labels.size().width;
 	int height = labels.size().height;
 
 	int memsize = width * height * sizeof(int);
 
-
-
-
-
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
     return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
 }
