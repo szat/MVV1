d3fea8f5dd1b7be8c01fcd57a4bca066aca3da8d
danielhogg <danhogg17@gmail.com>
2017-06-12 16:34:27 -0400

    added some comments

--- mvv_demo/core/merge_background.cpp
+++ mvv_demo/core/merge_background.cpp
@@ -11,6 +11,11 @@ using namespace std;
 using namespace cv;
 
 cv::Mat merge_images(cv::Mat img_1, cv::Mat img_2) {
+	/*
+	I question whether this will give good results for the background, as image stitching
+	usually assumes a stationary camera (which rotates its angle of view), but our use case
+	is two cameras placed at different locations (difference between convex and concave camera angles).
+	*/
 	vector<Mat> imgs = vector<Mat>();
 	imgs.push_back(img_1);
 	imgs.push_back(img_2);
