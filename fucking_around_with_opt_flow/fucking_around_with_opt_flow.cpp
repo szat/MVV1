d4c6e1dfd69fa339eee17066846968640b6a8886
szat <artabalt@gmail.com>
2017-06-18 10:32:19 -0400

    more_stupidity accepting optflow

---
+++ fucking_around_with_opt_flow/fucking_around_with_opt_flow.cpp
@@ -0,0 +1,11 @@
+// fucking_around_with_opt_flow.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
--- more_stupidity/kernel.cu
+++ more_stupidity/kernel.cu
@@ -11,23 +11,81 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/core/utility.hpp>
 
+#include <opencv2/video.hpp>
+#include <opencv2/optflow.hpp>
+
 //#include <opencv2/features2d/features2d.hpp>
 #include <AKAZE.h>
 #include <AKAZEConfig.h>
+//#include <cuda_profiler_api.h>
 #include <opencv2/calib3d.hpp> //AKAZE seems not to work without this
 
-int main() {
-	cv::Mat img1;
-	std::string img1_path = "..\\data_store\\images\\c1_img_000177.png";
-	img1 = cv::imread(img1_path);
-	if (img1.empty())
-	{
-		std::cout << "Could not open image..." << img1_path << "\n";
-		return -1;
-	}
+using namespace std;
+using namespace cv;
+using namespace libAKAZECU;
 
+int main() {
+	Mat img1;
+	string img1_path = "C:\\Users\\Adrian\\Documents\\GitHub\\mvv\\cuda_demo\\data_store\\disk1.jpg";
+	img1 = imread(img1_path);
+	if (img1.empty()) return -1;
+
+	Mat img2;
+	string img2_path = "C:\\Users\\Adrian\\Documents\\GitHub\\mvv\\cuda_demo\\data_store\\disk2.jpg";
+	img2 = imread(img2_path);
+	if (img2.empty()) return -1;
+
+	//TO DO: test all the different optical flow methods on disk1 / disk2
+	
+	Mat next1;
+	cvtColor(img1, next1, COLOR_BGR2GRAY);
+	Mat next2;
+	cvtColor(img2, next2, COLOR_BGR2GRAY);
+	Mat flow;
+
+	//Ptr<DenseOpticalFlow> createOptFlow_Farneback();
+	//calcOpticalFlowFarneback(next1, next2, flow, 0.4, 3, 12, 5, 8, 1.2, OPTFLOW_FARNEBACK_GAUSSIAN);
+	//calcOpticalFlowPyrLK(next1, next2, flow, 0.4, 3, 12, 5, 8, 1.2, OPTFLOW_FARNEBACK_GAUSSIAN);
+
+	calcOpticalFlowFarneback(next1, next2, flow, 0.4, 3, 12, 5, 8, 1.2, OPTFLOW_FARNEBACK_GAUSSIAN);
+
+
+
+
+
+	//So this works well
 	AKAZEOptions options;
-	libAKAZECU::AKAZE evolution1(options);
+
+	// Convert the image to float to extract features
+	Mat img1_32;
+	img1.convertTo(img1_32, CV_32F, 1.0 / 255.0, 0);
+	Mat img2_32;
+	img2.convertTo(img2_32, CV_32F, 1.0 / 255.0, 0);
+
+	// Don't forget to specify image dimensions in AKAZE's options
+	options.img_width = img1.cols;
+	options.img_height = img1.rows;
+
+	// Extract features
+	libAKAZECU::AKAZE evolution(options);
+	vector<KeyPoint> kpts1;
+	vector<KeyPoint> kpts2;
+	vector<vector<cv::DMatch> > dmatches;
+	Mat desc1;
+	Mat desc2;
+
+	evolution.Create_Nonlinear_Scale_Space(img1_32);
+	evolution.Feature_Detection(kpts1);
+	evolution.Compute_Descriptors(kpts1, desc1);
+
+	evolution.Create_Nonlinear_Scale_Space(img2_32);
+	evolution.Feature_Detection(kpts2);
+	evolution.Compute_Descriptors(kpts2, desc2);
+
+	Matcher cuda_matcher;
+
+	cuda_matcher.bfmatch(desc1, desc2, dmatches);
+	cuda_matcher.bfmatch(desc2, desc1, dmatches);
 
 	return 0;
 }
