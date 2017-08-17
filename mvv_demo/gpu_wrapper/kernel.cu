77f28f2dabfe6a4e374f2bf975563bd9473cf1c5
Daniel Hogg <danhogg17@gmail.com>
2017-08-17 13:10:34 -0400

    deleted empty project

--- mvv_demo/gpu_wrapper/kernel.cu
+++ mvv_demo/gpu_wrapper/kernel.cu
@@ -2,125 +2,114 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <iostream>
+#include <string>
 #include <stdio.h>
+#include <iostream>
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/utility.hpp>
+
+#include <opencv2/video.hpp>
+//#include <opencv2/optflow.hpp>
+
+//#include <opencv2/features2d/features2d.hpp>
+#include <AKAZE.h>
+#include <AKAZEConfig.h>
+//#include <cuda_profiler_api.h>
+#include <opencv2/calib3d.hpp> //AKAZE seems not to work without this
 
 using namespace std;
+using namespace cv;
+using namespace libAKAZECU;
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+const float MIN_H_ERROR = 5.00f;            ///< Maximum error in pixels to accept an inlier
+const float DRATIO = 0.80f;
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
+int main() {
+	VideoCapture cap_1("C:\\Users\\Danny\\Documents\\GitHub\\mvv\\data_store\\video\\judo_left.MP4");
+	if (!cap_1.isOpened()) {
+		cout << "Video 1 failed to load." << endl;
+		return -1;
+	}
 
-int main()
-{
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	VideoCapture cap_2("C:\\Users\\Danny\\Documents\\GitHub\\mvv\\data_store\\video\\judo_right.MP4");
+	if (!cap_2.isOpened()) {
+		cout << "Video 2 failed to load." << endl;
+		return -1;
+	}
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	int num_frames_1 = cap_1.get(CAP_PROP_FRAME_COUNT);
+	int num_frames_2 = cap_2.get(CAP_PROP_FRAME_COUNT);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	int start = 500;
+	int offset = 595;
+	cap_1.set(CV_CAP_PROP_POS_FRAMES, start + offset);
+	cap_2.set(CV_CAP_PROP_POS_FRAMES, start);
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	Mat img1;
+	cap_1.read(img1);
+	img1 = img1.clone();
+	Mat img2;
+	cap_2.read(img2);
+	img2 = img2.clone();
+
+	imwrite("..\\data_store\\images\\judo_left.png", img1);
+	imwrite("..\\data_store\\images\\judo_right.png", img2);
+
+	//So this works well
+	AKAZEOptions options;
+
+	// Convert the image to float to extract features
+	Mat img1_gray;
+	cvtColor(img1, img1_gray, CV_BGR2GRAY);
+	Mat img2_gray;
+	cvtColor(img2, img2_gray, CV_BGR2GRAY);
+	Mat img1_32;
+	img1_gray.convertTo(img1_32, CV_32F, 1.0 / 255.0, 0);
+	Mat img2_32;
+	img2_gray.convertTo(img2_32, CV_32F, 1.0 / 255.0, 0);
+
+	// Don't forget to specify image dimensions in AKAZE's options
+	options.img_width = img1.cols;
+	options.img_height = img1.rows;
+
+	// Extract features
+	libAKAZECU::AKAZE evolution(options);
+	vector<KeyPoint> kpts1;
+	vector<KeyPoint> kpts2;
+	vector<vector<cv::DMatch> > dmatches;
+	Mat desc1;
+	Mat desc2;
+
+	evolution.Create_Nonlinear_Scale_Space(img1_32);
+	evolution.Feature_Detection(kpts1);
+	evolution.Compute_Descriptors(kpts1, desc1);
+
+	evolution.Create_Nonlinear_Scale_Space(img2_32);
+	evolution.Feature_Detection(kpts2);
+	evolution.Compute_Descriptors(kpts2, desc2);
+
+	Matcher cuda_matcher;
+
+	cuda_matcher.bfmatch(desc1, desc2, dmatches);
+	cuda_matcher.bfmatch(desc2, desc1, dmatches);
+
+	vector<cv::Point2f> matches, inliers;
+
+	matches2points_nndr(kpts2, kpts1, dmatches, matches, DRATIO);
+	compute_inliers_ransac(matches, inliers, MIN_H_ERROR, false);
+
+	Mat img_com = cv::Mat(cv::Size(img1.cols * 2, img1.rows), CV_8UC3);
+	draw_keypoints(img1, kpts1);
+	draw_keypoints(img2, kpts2);
+	draw_inliers(img1, img2, img_com, inliers);
+	cv::namedWindow("Inliers", cv::WINDOW_NORMAL);
+	cv::imshow("Inliers", img_com);
+	cv::waitKey(0);
 
 	cin.get();
 
-    return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+	return 0;
 }
