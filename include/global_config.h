4b63615339ad1b0d3336538df2a3120652a96cea
Daniel Hogg <danhogg17@gmail.com>
2017-08-21 11:54:56 -0400

    deleted comments, added error codes

--- include/global_config.h
+++ include/global_config.h
@@ -9,7 +9,7 @@
 int get_video_width() {
 	INIReader reader(SETTINGS_PATH);
 	if (reader.ParseError() < 0) {
-		std::cout << "Can't load 'settings.ini'\n";
+		std::cout << "ERROR CODE 001: Can't load 'settings.ini'\n";
 		return 0;
 	}
 	return reader.GetInteger("user", "video_width", 0);
@@ -18,7 +18,7 @@ int get_video_width() {
 int get_video_height() {
 	INIReader reader(SETTINGS_PATH);
 	if (reader.ParseError() < 0) {
-		std::cout << "Can't load 'settings.ini'\n";
+		std::cout << "ERROR CODE 001: Can't load 'settings.ini'\n";
 		return 0;
 	}
 	return reader.GetInteger("user", "video_height", 0);
